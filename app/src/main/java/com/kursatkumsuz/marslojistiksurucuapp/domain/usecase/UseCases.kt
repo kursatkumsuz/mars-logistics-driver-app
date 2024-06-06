5ce6e1ecaaeb3501bcf6bd6d3b189458c2636374
kursatkumsuz <kursatkumsuz@gmail.com>
2024-06-06 14:06:24 +0300

    Complete UseCases.kt & Response.kt

---
+++ app/src/main/java/com/kursatkumsuz/marslojistiksurucuapp/domain/usecase/UseCases.kt
@@ -0,0 +1,13 @@
+package com.kursatkumsuz.marslojistiksurucuapp.domain.usecase
+
+import com.kursatkumsuz.marslojistiksurucuapp.domain.usecase.signin_usecase.SaveOnBoardingStateUseCase
+import com.kursatkumsuz.marslojistiksurucuapp.domain.usecase.signin_usecase.SignInWithEmailAndPasswordUseCase
+import com.kursatkumsuz.marslojistiksurucuapp.domain.usecase.splash_usecases.IsSignedInUseCase
+import com.kursatkumsuz.marslojistiksurucuapp.domain.usecase.splash_usecases.ReadOnBoardingStateUseCase
+
+data class UseCases (
+    val saveOnBoardingStateUseCase: SaveOnBoardingStateUseCase,
+    val readOnBoardingStateUseCase: ReadOnBoardingStateUseCase,
+    val signInWithEmailAndPasswordUseCase: SignInWithEmailAndPasswordUseCase,
+    val isSignedInUseCase: IsSignedInUseCase
+)
